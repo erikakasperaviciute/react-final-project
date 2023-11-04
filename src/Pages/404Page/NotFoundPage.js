e3b7de6cd30b5ef2f76aa14741c1a1736aaa7533
erikakasperaviciute <erika.kasperaviciute@hotmail.lt>
2023-11-05 00:23:23 +0200

    404 page

---
+++ src/Pages/404Page/NotFoundPage.js
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import Container from "../../Components/Container/Container";
+import styles from "./NotFoundPage.module.scss";
+
+function NotFoundPage() {
+  return (
+    <Container>
+      <div className={styles.contentWrapper}>
+        <h1>404</h1>
+        <h2>You have found a secret place.</h2>
+        <p>
+          Unfortunately, this is only a 404 page. You may have mistyped the
+          address, or the page has been moved to another URL.
+        </p>
+        <div className={styles.backHomeBtn}>
+          <Link to="/">Take me back to home page</Link>
+        </div>
+      </div>
+    </Container>
+  );
+}
+export default NotFoundPage;
